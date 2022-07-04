614ab9054772153d804e3bf138d89d06aaa84082
cecilia-tisca <ceciliatisca@gmail.com>
2022-07-04 12:52:21 -0500

    Feat: Project creation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+
+import { useEffect, useState } from "react";
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <h1>POKEMON </h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+
+        </div>
+        <button className="next">Next</button>
+        <button className="previous">Previous</button>
+      </div>
     </div>
   );
 }
