a43d8275734c58a7a25249fb40dd6e64fe3d02af
cecilia-tisca <ceciliatisca@gmail.com>
2022-07-06 11:11:09 -0500

    Feat: Student information added

--- src/routes/matricula.jsx
+++ src/routes/matricula.jsx
@@ -4,6 +4,10 @@ export default function Matricula() {
         <Navbar />
         <main style={{ padding: "1rem 0" }}>
             <h2>Matricula</h2>
+            <p>Diana Cecilia Tiscareño Reyes</p>
+            <p>Matricula: 190184</p>
+            <p>Ingeniería en Desarrollo y Gestion de Software</p>
+            <p>UNIVERSIDAD TECNOLÓGICA DE AGUASCALIENTES</p>
         </main>
         </div>
     )
