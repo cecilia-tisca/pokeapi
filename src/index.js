7dfab2c6915bc33ca40b796695c54ce12ef80df4
cecilia-tisca <ceciliatisca@gmail.com>
2022-07-06 10:51:01 -0500

    Feat: We place the path and call its components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from "react-router-dom;"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './index.css';
 import App from './App';
-import Matricula from "./routes/matricula"
-import PokemonId from "./routes/pokemonId"
-import Pokemons from "./routes/pokemons"
+import Matricula from './routes/matricula';
+import PokemonId from './routes/pokemonId';
+import Pokemons from './routes/pokemons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element = {<App />}></Route>
+        <Route path="/" element = {<App />} />
+        <Route path="/matricula" element={<Matricula />} />
+        <Route path="/pokemonId" element={<PokemonId />} />
+        <Route path="/pokemons" element={<Pokemons />} />
+    
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
