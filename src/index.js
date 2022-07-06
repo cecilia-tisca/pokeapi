2097f104ec34bc822e13b02c671067dec5d78905
cecilia-tisca <ceciliatisca@gmail.com>
2022-07-06 10:09:20 -0500

    -

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom;"
 import './index.css';
 import App from './App';
-import Matricula from "./routes/matricula"
-import PokemonId from "./routes/pokemonId"
-import Pokemons from "./routes/pokemons"
+import Matricula from './routes/matricula';
+import PokemonId from './routes/pokemonId';
+import Pokemons from './routes/pokemons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element = {<App />}></Route>
+        <Route path="/" element = {<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
