c3caf7042ccb4afb125969d7875d877f111cad96
cecilia-tisca <ceciliatisca@gmail.com>
2022-07-06 09:41:39 -0500

    Feat: Routes and Route were imported

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom;"
+import {BrowserRouter, Routes, Route} from "react-router-dom;"
 import './index.css';
 import App from './App';
 import Matricula from "./routes/matricula"
 import PokemonId from "./routes/pokemonId"
 import Pokemons from "./routes/pokemons"
-import { Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
