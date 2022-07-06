63c2294cd0365f4fb600761edf472d5b42abeae4
cecilia-tisca <ceciliatisca@gmail.com>
2022-07-06 09:28:08 -0500

    Feat: the routes were imported and placed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from "react-router-dom;"
 import './index.css';
 import App from './App';
+import Matricula from "./routes/matricula"
+import PokemonId from "./routes/pokemonId"
+import Pokemons from "./routes/pokemons"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+     <Route path="/" element = {<App />}></Route>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
