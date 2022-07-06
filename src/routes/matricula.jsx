997d3ee214c80b9d39f401cccacbc6818db943f9
cecilia-tisca <ceciliatisca@gmail.com>
2022-07-06 10:57:51 -0500

    Feat: navbar added

--- src/routes/matricula.jsx
+++ src/routes/matricula.jsx
@@ -1,8 +1,11 @@
 export default function Matricula() {
     return (
+        <div>
+        <Navbar />
         <main style={{ padding: "1rem 0" }}>
             <h2>Matricula</h2>
         </main>
+        </div>
     )
     
 }
