fc9667589ebc5d42e0c306435503b17388836f81
cecilia-tisca <ceciliatisca@gmail.com>
2022-07-06 01:21:28 -0500

    Feat: file creation

---
+++ src/paginas/pok.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+import { UseFetch } from "../components/fetch";
+
+const Pokemon = () => {
+    const [url,setUrl]=useState('https://pokeapi.co/api/v2/pokemon')
+    const estado=UseFetch(url)
+    const {cargando,data}=estado
+    cargando? console.log('cargando'):console.log(data.results);
+
+    return(
+        <div>
+            <h1>
+                Página de Pokemon
+            </h1>
+        </div>
+    )
+}
