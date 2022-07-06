b3f6fa7f47be6db3dec9dc786034cf6da6181523
cecilia-tisca <ceciliatisca@gmail.com>
2022-07-06 01:50:36 -0500

    Feat: Added links to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,14 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
       <div>
         <h1>POKEAPI </h1>
+        <Link to="/pokemons">Pokemons</Link> |{""}
+        <Link to="/pokemon/:pokemonId">Pokemon Id</Link> |{""}
+        <Link to="matricula">Matricula</Link>
       </div>
     </nav>
   );
